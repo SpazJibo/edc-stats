fbb189bc8ee9b6c48b9f76ad9acb3508a8f17f3f
spazjibo <curtjibo@gmail.com>
2025-01-18 12:38:25 -0600

    Adding min and max evp ratings to search.

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -32,16 +32,86 @@ export async function GET(req: Request) {
   };
 
   const playerData = await getData();
-  const search = searchParams?.get("query")?.toLowerCase();
-  if (search && search !== "") {
-    const foundPlayers = playerData.filter((player) =>
-      player.name.toLowerCase().includes(search),
-    );
+  const name = searchParams?.get("name")?.toLowerCase();
+  const maxQuery = searchParams?.get("max");
+  const max = maxQuery ? Number.parseFloat(maxQuery) : null;
+  const minQuery = searchParams?.get("min");
+  console.log("!!! minQuery: ", minQuery);
+  const min = minQuery ? Number.parseFloat(minQuery) : null;
 
-    if (foundPlayers && foundPlayers.length > 0) {
-      return NextResponse.json(foundPlayers);
+  const foundPlayers: IPlayer[] = [];
+
+  console.log("!!! playerData.length: ", playerData.length);
+  console.log("!!! name: ", name);
+  console.log("!!! min: ", min);
+  console.log("!!! max: ", max);
+
+  if (name && name !== "") {
+    playerData
+      .filter((player) => player.name.toLowerCase().includes(name))
+      .map((player) => foundPlayers.push(player));
+  } else {
+    console.log("!!! else");
+    console.log("!!! playerData: ", playerData.length);
+    foundPlayers.concat(playerData);
+  }
+
+  console.log("!!! foundPlayers before max & min: ", foundPlayers.length);
+  if (max && min) {
+    console.log(
+      "!!! max && min playersData: ",
+      playerData.length,
+      " foundPlaysers: ",
+      foundPlayers.length,
+    );
+    const evpPlayers =
+      foundPlayers.length > 0
+        ? foundPlayers.filter((player) => {
+            return player.evpRating <= max && player.evpRating >= min;
+          })
+        : playerData.filter((player) => {
+            return player.evpRating <= max && player.evpRating >= min;
+          });
+
+    console.log("!!! evpPlayers in min && max: ", evpPlayers);
+    if (evpPlayers && evpPlayers.length > 0) {
+      return NextResponse.json(evpPlayers);
     } else {
       return NextResponse.json([]);
     }
   }
+
+  if (min) {
+    const evpPlayers =
+      foundPlayers.length > 0
+        ? foundPlayers.filter((player) => {
+            return player.evpRating >= min;
+          })
+        : playerData.filter((player) => {
+            return player.evpRating >= min;
+          });
+    if (evpPlayers && evpPlayers.length > 0) {
+      return NextResponse.json(evpPlayers);
+    } else {
+      return NextResponse.json([]);
+    }
+  }
+
+  if (max) {
+    const evpPlayers =
+      foundPlayers.length > 0
+        ? foundPlayers.filter((player) => {
+            return player.evpRating <= max;
+          })
+        : playerData.filter((player) => {
+            return player.evpRating <= max;
+          });
+    if (evpPlayers && evpPlayers.length > 0) {
+      return NextResponse.json(evpPlayers);
+    } else {
+      return NextResponse.json([]);
+    }
+  }
+  // Default Return
+  return NextResponse.json(foundPlayers);
 }
--- src/app/components/searchForm.tsx
+++ src/app/components/searchForm.tsx
@@ -5,17 +5,24 @@ import { IPlayer } from "../lib/IPlayer";
 
 export const SearchForm = () => {
   const [data, setData] = useState<IPlayer[]>();
-  const [search, setSearch] = useState("");
+  const [name, setName] = useState("");
+  const [minEvp, setMinEvp] = useState("");
+  const [maxEvp, setMaxEvp] = useState("");
   const [loading, setLoading] = useState(false);
 
   const searchAction = async () => {
+    console.log("!!! searchAction");
     if (!loading) {
       setData([]);
       setLoading(true);
-      if (search && search.length > 0) {
-        const response = await fetch(
-          `/api/search?query=${encodeURIComponent(search)}`,
-        );
+      if (
+        (name && name.length > 0) ||
+        (minEvp && minEvp.length > 0) ||
+        (maxEvp && maxEvp.length > 0)
+      ) {
+        const url = `/api/search?name=${encodeURIComponent(name)}&min=${encodeURIComponent(minEvp)}&max=${encodeURIComponent(maxEvp)}`;
+        console.log("!!! url: ", url);
+        const response = await fetch(url);
         console.log("!!! before Request");
         const data = await response.json();
         console.log("!!! after Request");
@@ -35,62 +42,86 @@ export const SearchForm = () => {
       <h1 className="mx-5">EDC Remote Stat Search</h1>
       <Form action={handleClick}>
         <div>
-          <input
-            name="search"
-            autoFocus
-            onChange={(e) => setSearch(e.target.value)}
-            placeholder="Enter Name"
-            className="mx-5 px-3 text-slate-800 focus:outline-none"
-            value={search}
-            tabIndex={0}
-          />
-          <button
-            type="submit"
-            className="bg-purple-500 hover:bg-purple-700 text-white font-bold px-2 border-purple-900 outline-none rounded"
-          >
-            Search
-          </button>
+          <div className="mt-3 w-full">
+            <input
+              name="name"
+              autoFocus
+              onChange={(e) => setName(e.target.value)}
+              placeholder="Enter Name"
+              className="mx-5 px-3 text-slate-800 focus:outline-none"
+              value={name}
+              tabIndex={0}
+            />
+          </div>
+          <div className="mt-3 w-full">
+            <input
+              name="minEvp"
+              onChange={(e) => setMinEvp(e.target.value)}
+              placeholder="Enter Minimum Evp Rating"
+              className="mx-5 px-3 text-slate-800 focus:outline-none"
+              value={minEvp}
+              tabIndex={0}
+            />
+          </div>
+          <div className="mt-3 w-full">
+            <input
+              name="maxEvp"
+              onChange={(e) => setMaxEvp(e.target.value)}
+              placeholder="Enter Max Evp Rating"
+              className="mx-5 px-3 text-slate-800 focus:outline-none"
+              value={maxEvp}
+              tabIndex={0}
+            />
+          </div>
+          <div className="mt-3 w-full">
+            <button
+              type="submit"
+              className="mx-5 w-56 bg-purple-500 hover:bg-purple-700 text-white font-bold px-2 border-purple-900 outline-none rounded"
+            >
+              Search
+            </button>
+          </div>
         </div>
       </Form>
 
       {loading ? (
         <div className="mx-5 my-5">Loading....</div>
       ) : (
-        <table
-          className={"table-auto w-full mx-3 my-5 border-gray-600 border-2"}
-        >
-          <thead className="bg-gradient-to-r from-purple-900 to-pink-400 text-white">
-            <tr className="font-bold">
-              <td className={"w-auto p-3"}>Player Name</td>
-              <td className={"w-auto p-3"}>EVP Rating</td>
-              <td className={"w-auto p-3"}>PPD</td>
-              <td className={"w-auto p-3"}>MPR</td>
-            </tr>
-          </thead>
-          <tbody>
-            {data?.map((player: IPlayer, index) => {
-              return (
-                <tr
-                  key={index}
-                  className="even:bg-purple-200 odd:bg-gray-100 even:text-white-900 dark:text-gray-900"
-                >
-                  <td className={"w-auto text-sm text-left p-2"}>
-                    {player.name}
-                  </td>
-                  <td className={"w-auto text-sm text-left p-2"}>
-                    {player.evpRating.toFixed(2)}
-                  </td>
-                  <td className={"w-auto text-sm text-left p-2"}>
-                    {player.ppd.toFixed(2)}
-                  </td>
-                  <td className={"w-auto text-sm text-left p-2"}>
-                    {player.mpr.toFixed(2)}
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+        <div className="inline-block rounded-lg border shadow-2xl my-5 mx-5">
+          <table className={"table-auto w-full"}>
+            <thead className="bg-gradient-to-r from-purple-900 to-pink-400 text-white outline outline-1 outline-purple-900 overflow-hidden">
+              <tr className="font-bold">
+                <th className={"w-auto text-left"}>Player Name</th>
+                <th className={"w-auto text-left "}>EVP Rating</th>
+                <th className={"w-auto text-left"}>PPD</th>
+                <th className={"w-auto text-left"}>MPR</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data?.map((player: IPlayer, index) => {
+                return (
+                  <tr
+                    key={index}
+                    className="even:bg-purple-200 odd:bg-gray-100 even:text-white-900 dark:text-gray-900 "
+                  >
+                    <td className={"w-auto text-sm text-left p-2"}>
+                      {player.name}
+                    </td>
+                    <td className={"w-auto text-sm text-left p-2"}>
+                      {player.evpRating.toFixed(2)}
+                    </td>
+                    <td className={"w-auto text-sm text-left p-2"}>
+                      {player.ppd.toFixed(2)}
+                    </td>
+                    <td className={"w-auto text-sm text-left p-2"}>
+                      {player.mpr.toFixed(2)}
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       )}
     </div>
   );
